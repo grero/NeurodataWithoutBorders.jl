6b0faa182962966033ca29ea462a0e5d65998388
roger <roger.herikstad@gmail.com>
2016-09-05 10:04:28 +0800

    Basic write/load now works for Electrialseries

--- src/io.jl
+++ src/io.jl
@@ -1,15 +1,50 @@
 using FileIO
 import Base.write
 
-function FileIO.load(file::File{DataFormat{:NWB}})
+function FileIO.load(file::File{DataFormat{:NWB}})::NWBData
 	#TODO: Read the hdf5 file, iterating through each group and classifying the datasets in that group according to the identified neurodata_type
 	#e.g. neurodata_type == TimeSeries indicate that the datasets in this groups describes a time series object
 	file = HDF5.h5open(file.filename)
 	if "acquisition" in names(file)
-		if "timeseries" in names(file["acquisition"])
-			for g in names(file["acquisition"]["timeseries"])
-				_data = read(file["acquisition"]["timeseries"][g]["data"])
-				_timestamps = read(file["acquisition"]["timeseries"][g]["timestamps"])
+		g = file["acquisition"]
+		if "timeseries" in names(g)
+			gg = g["timeseries"]
+			for ggg in gg
+				_ancestry = read(ggg,"ancestry")
+				#Check the last entry
+				_datatype = _ancestry[end]
+				_data = read(ggg["data"])
+				if "timestamps" in ggg
+					__timestamps = read(ggg["timestamps"])
+				elseif "start_time" in ggg && "rate" in ggg
+					_start_time = read(ggg,"start_time")
+					_rate = read(ggg, "rate")
+					_timestamps = range(start_time*SIUnits.Second,(1/_rate)*SIUnits.Second,size(_data,1))
+				else
+					warn("No timestanps found. Using dummy timestamps")
+					_timestamps = range(1.0*SIUnits.Second, 1.0*SIUnits.Second, size(_data,1))
+				end
+				_help = read(ggg["help"])
+				_resolution = read(ggg["resolution"])
+				_electrode_idx = read(ggg["electrode_idx"])
+				_name = read(ggg["name"])
+				_unit = read(ggg["unit"])
+				#stop gap until SIUnits can parse derived units
+				#unit = parse(SIUnits.SIUnit, _unit)
+				if _unit == "V"
+					#unit = VoltageType
+					unit = SIUnits.ShortUnits.V
+				elseif _unit == "kg m²s⁻³A⁻¹"
+					unit = SIUnits.ShortUnits.V
+				elseif _unit == "mV"
+					unit = SIUnits.ShortHunits.mV
+				else
+					warn("Unknonw units. Defaulting to V")
+					unit = SIUnits.ShortUnits.V
+				end
+				if _datatype == "ElectricalSeries"
+					return ElectricalSeries(_data*unit,_help, _resolution, _electrode_idx, _name, _timestamps)
+				end
 			end
 		end
 	end
@@ -31,10 +66,15 @@ function write(s::HDF5.DataFile, data::TimeSeries)
 	end
 	write(s, "$(_path)/name", data.name)
 	write(s, "$(_path)/help", data.help)
-	write(s, "$(_path)/resoluation", data.resolution)
+	write(s, "$(_path)/resolution", data.resolution)
 	write(s, "$(_path)/unit",string(SIUnits.unit(first(data.data))))
 	write(s, "$(_path)/electrode_idx", data.electrode_idx)
-end
-
-function read(s::HDF5.DataFile) 
+	if typeof(data.timestamps) <: Range
+		write(s, "$(_path)/start_time", first(data.timestamps))
+		_rate = 1.0/(data.timestamps[2] - data.timestamps[1])
+		write(s, "$(_path)/rate", _rate)
+	else
+		write(s, "$(_path)/timestamps", data.timestamps)
+	end
+	write(s, "$(_path)/ancestry", ["TimeSeries", split(string(typeof(data)),".")[end]])
 end
--- src/types.jl
+++ src/types.jl
@@ -1,3 +1,4 @@
+import Base.parse, Base.range
 abstract NWBData
 abstract Acquisition <: NWBData
 abstract Analysis <: NWBData
@@ -8,6 +9,10 @@ abstract Stimulus <: NWBData
 abstract TimeSeries <: Acquisition
 
 typealias VoltageType SIUnits.SIQuantity{Float64, 2,1,-3,-1,0,0,0,0,0}
+typealias TimeRangeType SIUnits.SIRange{FloatRange{Float64},Float64,0,0,1,0,0,0,0,0,0} 
+typealias TimeArrayType Array{SIUnits.SIQuantity{Float64,0,0,1,0,0,0,0,0,0},2}
+
+range{T<:SIUnits.SIQuantity}(a::T, b::T, n::Integer) = a:b:(a+(n-1)*b)
 
 type ElectricalSeries <: TimeSeries
 	data::Array{VoltageType,2}
@@ -15,6 +20,13 @@ type ElectricalSeries <: TimeSeries
 	resolution::Float64
 	electrode_idx::Array{Int64,1}
 	name::ASCIIString
+	timestamps::Union{TimeRangeType, TimeArrayType}
+end
+
+function parse(::Type{SIUnits.SIUnit}, s::String)
+	if s == "mV"
+		return SIUnits.ShortUnits.mV
+	end
 end
 
 
