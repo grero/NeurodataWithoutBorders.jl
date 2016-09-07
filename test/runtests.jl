13fc493197c2e10e7ee03ae2ee00ffcae20db6e8
roger <roger.herikstad@gmail.com>
2016-09-07 13:35:06 +0800

    Adds tests

---
+++ test/runtests.jl
@@ -0,0 +1,21 @@
+import NeurodataWithoutBorders
+using FileIO
+using Base.Test
+using SIUnits
+using SIUnits.ShortUnits
+
+
+dir = tempdir()
+aname = joinpath(dir, "test3.nwb")
+
+B = rand(1000,10)mV
+eldata = NeurodataWithoutBorders.ElectricalSeries(B, "test", 1.0, collect(1:10), "test3",NeurodataWithoutBorders.range(0.0s, (1/30000.0)s, size(B,1)))
+NeurodataWithoutBorders.save(File(format"NWB",fname), eldata)
+wdata = NeurodataWithoutBorders.load(File(format"NWB", fname))
+
+#rm(fname)
+@test eldata.data == wdata.data
+@test eldata.help == wdata.help
+@test eldata.timestamps == wdata.timestamps
+
+
